605947bf030231176d403852e6ff6813604d1e81
_harshbutle09 <harshbutle09@gmail.com>
2025-05-06 20:10:14 +0530

    nQueens optimized and BruteForce added

---
+++ nQueens.cpp
@@ -0,0 +1,88 @@
+// Following problem is to solve the classical example of N-Queens
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool isSafe(int row , int col , vector<string>& Board , int n){
+    int i = row;
+    int j = col;
+    
+    // check for upper right diagonal
+    while(i > -1 && j < n){
+        if(Board[i][j] == 'Q'){
+            return 0;
+        }
+        i --;
+        j ++;
+    }
+    // check for upper left diagonal
+    i = row;
+    j = col;
+    while(i > -1 && j > -1){
+        if(Board[i][j] == 'Q'){
+            return 0;
+        }
+        i --;
+        j --;
+    }
+    return 1;
+}
+void find(int row , int n , vector<vector<string>>& ans , vector<string>& Board , vector<bool>& column ){
+    if(row == n){
+        ans.push_back(Board);
+        return;
+    }
+
+    for(int j = 0 ; j < n ; j ++){
+        if(column[j] == 0 && isSafe(row , j , Board , n )){
+            Board[row][j] = 'Q';
+            column[j] = 1;
+            find(row + 1 , n , ans , Board , column);
+            Board[row][j] = '.';
+            column[j] = 0;
+        }
+    }
+}
+vector<vector<string>> nQueens(int n){
+    vector<vector<string>> ans;
+    vector<string>Board(n);
+
+    for(int i = 0 ; i < n ; i ++){
+        for(int j = 0 ; j < n ; j ++){
+            Board[i].push_back('.');
+        }
+    }
+    vector<bool> column(n , 0);
+
+    find(0 , n , ans , Board , column);
+    return ans;
+
+}
+void printBoard(vector<string>Board , int n){
+    for(int i = 0 ; i < Board.size() ; i ++){
+        for(int j = 0 ; j < n ; j ++){
+            cout << Board[i][j]<<" ";
+        }cout << endl;
+    }
+}
+int main(){
+    int n;
+    cout << "Enter value of n \n";
+    cin >> n;
+    vector<vector<string>> ans = nQueens(n);
+    if(ans.size() == 0){
+        cout << "There are no possible solutions for 'n' equals to "<< n << endl;
+        return 0;
+    }
+    cout << "Following are all the possible solutions: " << endl;
+
+    for(int i = 0 ; i < ans.size() ; i ++){
+        vector<string>Board = ans[i];
+        printBoard(Board , n);
+
+        cout << endl << endl;
+    }
+    return 0;
+}
---
+++ nQueensOptimized.cpp
@@ -0,0 +1,77 @@
+// Following problem is to solve the classical example of N-Queens
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+// bool isSafe(int row , int col , vector<string>& Board , int n , vector<bool>& rightDiagonal , vector<bool>& leftDiagonal){
+//     if(leftDiagonal[col - row + n - 1] == 1 || rightDiagonal[row + col] == 1){
+//         return 0;
+//     }
+//     return 1;
+// }
+
+
+void find(int row , int n , vector<vector<string>>& ans , vector<string>& Board , vector<bool>& column , vector<bool>& rightDiagonal , vector<bool>& leftDiagonal ){
+    if(row == n){
+        ans.push_back(Board);
+        return;
+    }
+
+    for(int j = 0 ; j < n ; j ++){
+        if(column[j] == 0 && (leftDiagonal[j - row + n - 1] == 0 && rightDiagonal[j + row] == 0)){
+            Board[row][j] = 'Q';
+            column[j] = 1;
+            leftDiagonal[j - row + n - 1] = 1;
+            rightDiagonal[j + row] = 1;
+            find(row + 1 , n , ans , Board , column , rightDiagonal , leftDiagonal);
+            leftDiagonal[j - row + n - 1] = 0;
+            rightDiagonal[j + row] = 0;
+            Board[row][j] = '.';
+            column[j] = 0;
+        }
+    }
+}
+vector<vector<string>> nQueens(int n){
+    vector<vector<string>> ans;
+    vector<string>Board(n);
+
+    for(int i = 0 ; i < n ; i ++){
+        for(int j = 0 ; j < n ; j ++){
+            Board[i].push_back('.');
+        }
+    }
+    vector<bool> column(n , 0);
+    vector<bool> rightDiagonal( 2 * n - 1  , 0);
+    vector<bool> leftDiagonal(2 * n - 1 , 0);
+    find(0 , n , ans , Board , column , rightDiagonal , leftDiagonal);
+    return ans;
+
+}
+void printBoard(vector<string>Board , int n){
+    for(int i = 0 ; i < Board.size() ; i ++){
+        for(int j = 0 ; j < n ; j ++){
+            cout << Board[i][j]<<" ";
+        }cout << endl;
+    }
+}
+int main(){
+    int n;
+    cout << "Enter value of n \n";
+    cin >> n;
+    vector<vector<string>> ans = nQueens(n);
+    if(ans.size() == 0){
+        cout << "There are no possible solutions for 'n' equals to "<< n << endl;
+        return 0;
+    }
+    cout << "Following are all the possible solutions: " << endl;
+
+    for(int i = 0 ; i < ans.size() ; i ++){
+        vector<string>Board = ans[i];
+        printBoard(Board , n);
+
+        cout << endl << endl;
+    }
+    return 0;
+}
